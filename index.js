09475d8c78a177f77c395521ddbfe80898e20947
Mirek Chowaniok <mirek@gmail.com>
2018-05-31 13:03:51 -0400

    change fastify

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 require('dotenv').config()
+const log = require('pino')({ level: 'info' })
+const app = require('fastify')()
 // const fastifyEnv = require('fastify-env')
 const helloHandler = require('./handlers/helloHandler')
 
@@ -23,18 +25,20 @@ const opts = {}
 opts.port = process.env.PORT || 3000
 
 opts.logger = {
-  level: process.env.LOGGER || 'warn'
+  level: 'warn'
 }
 
-const app = require('fastify')(opts)
+//const app = require('fastify')(opts)
 // fastify.register(fastifyEnv, options, err => {
 //   console.log(fastify.config)
 // })
 app.register(require('./service/health'))
+app.register(require('./service/test'))
 
 const start = async opts => {
   try {
-    await app.listen(opts.port, '0.0.0.0')
+    await app.listen(3000, '::')
+    app.log.info(`server listeing on ${app.server.address().port}`)
   } catch (err) {
     app.log.error(err)
     process.exit(1)
